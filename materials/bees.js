ed3530d5b80ada680c0fb52fbf7d1a13e854e54a
Miles Bader <milesdbader@gmail.com>
2021-07-07 15:43:24 -0700

    made BEES array in materials folder and updated preload to pull data from said array

---
+++ materials/bees.js
@@ -0,0 +1,114 @@
+var BEES = [
+  {
+    image: 'bees-with-color/bee-A1DF.svg',
+    antennae: 'A',
+    wings: '1',
+    pattern: 'D',
+    legs: 'F'
+  },
+  {
+    image: 'bees-with-color/bee-A1DM.svg',
+    antennae: 'A',
+    wings: '1',
+    pattern: 'D',
+    legs: 'M'
+  },
+  {
+    image: 'bees-with-color/bee-A1SF.svg',
+    antennae: 'A',
+    wings: '1',
+    pattern: 'S',
+    legs: 'F'
+  },
+  {
+    image: 'bees-with-color/bee-A1SM.svg',
+    antennae: 'A',
+    wings: '1',
+    pattern: 'S',
+    legs: 'M'
+  },
+  {
+    image: 'bees-with-color/bee-A2DF.svg',
+    antennae: 'A',
+    wings: '2',
+    pattern: 'D',
+    legs: 'F'
+  },
+  {
+    image: 'bees-with-color/bee-A2DM.svg',
+    antennae: 'A',
+    wings: '2',
+    pattern: 'D',
+    legs: 'M'
+  },
+  {
+    image: 'bees-with-color/bee-A2SF.svg',
+    antennae: 'A',
+    wings: '2',
+    pattern: 'S',
+    legs: 'F'
+  },
+  {
+    image: 'bees-with-color/bee-A2SM.svg',
+    antennae: 'A',
+    wings: '2',
+    pattern: 'S',
+    legs: 'M'
+  },
+  {
+    image: 'bees-with-color/bee-B1DF.svg',
+    antennae: 'B',
+    wings: '1',
+    pattern: 'D',
+    legs: 'F'
+  },
+  {
+    image: 'bees-with-color/bee-B1DM.svg',
+    antennae: 'B',
+    wings: '1',
+    pattern: 'D',
+    legs: 'M'
+  },
+  {
+    image: 'bees-with-color/bee-B1SF.svg',
+    antennae: 'B',
+    wings: '1',
+    pattern: 'S',
+    legs: 'F'
+  },
+  {
+    image: 'bees-with-color/bee-B1SM.svg',
+    antennae: 'B',
+    wings: '1',
+    pattern: 'S',
+    legs: 'M'
+  },
+  {
+    image: 'bees-with-color/bee-B2DF.svg',
+    antennae: 'B',
+    wings: '2',
+    pattern: 'D',
+    legs: 'F'
+  },
+  {
+    image: 'bees-with-color/bee-B2DM.svg',
+    antennae: 'B',
+    wings: '2',
+    pattern: 'D',
+    legs: 'M'
+  },
+  {
+    image: 'bees-with-color/bee-B2SF.svg',
+    antennae: 'B',
+    wings: '2',
+    pattern: 'S',
+    legs: 'F'
+  },
+  {
+    image: 'bees-with-color/bee-B2SM.svg',
+    antennae: 'B',
+    wings: '2',
+    pattern: 'S',
+    legs: 'M'
+  }
+]
