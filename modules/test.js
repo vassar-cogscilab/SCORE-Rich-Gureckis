ad8ff83b808520f64c6ba2bc694471fa0acd4d96
Miles Bader <milesdbader@gmail.com>
2021-07-08 14:54:03 -0700

    modified actual test code to work with rebuild; added 32 and 64 set builders

--- modules/test.js
+++ modules/test.js
@@ -145,8 +145,13 @@ var sample_obj = {
     pattern: 'X',
     legs: 'F'
   }
+
+  Returns an array of 16 bee objects. Bees are:
+    -> Each represented in the set of 16
+    -> Balanced such that there are exactly 2 evil bees in each set of 8
+    -> Coded as "good" or "evil", under the "alignment" attribute
 */
-function bee_chooser(bee_group_object) {
+function bee_16set_builder(bee_group_object) {
   // move relevant info into rel_info array, as an array of objects
   var rel_info = [];
   for (const [dim, val] of Object.entries(bee_group_object)) {
@@ -162,7 +167,6 @@ function bee_chooser(bee_group_object) {
   var shuffled_bees = jsPsych.randomization.repeat(BEES, 1); //
   var bad_bees = shuffled_bees.filter(bee => bee[rel_info[0].dimension] == rel_info[0].value && bee[rel_info[1].dimension] == rel_info[1].value);
   var good_bees = shuffled_bees.filter(bee => !(bee[rel_info[0].dimension] == rel_info[0].value && bee[rel_info[1].dimension] == rel_info[1].value));
-
   bad_bees.forEach(bee => bee['alignment'] = 'evil');
   good_bees.forEach(bee => bee['alignment'] = 'good');
 
@@ -171,34 +175,39 @@ function bee_chooser(bee_group_object) {
   var first_eight_randomized = jsPsych.randomization.repeat(first_eight,1);
   var second_eight_randomized = jsPsych.randomization.repeat(second_eight,1);
   var full_list = [].concat(first_eight_randomized, second_eight_randomized);
+  console.log(full_list);
   return full_list
 }
 
-console.log(bee_chooser(rel_dim_1,rel_dim_2));
+function bee_32set_builder(bee_group_object){
+  var first_16 = bee_16set_builder(bee_group_object);
+  var second_16 = bee_16set_builder(bee_group_object);
+  return first_16.concat(second_16);
+}
 
+function bee_64set_builder(bee_group_object){
+  var first_32 = bee_32set_builder(bee_group_object);
+  var second_32 = bee_32set_builder(bee_group_object);
+  return first_32.concat(second_32);
+}
 
 
 var actual_trial = {
   timeline: [
       {
           type: 'image-button-response',
-          stimulus: jsPsych.timelineVariable(bee_chooser),
+          stimulus: jsPsych.timelineVariable('image'),
           choices: ['Harvest', 'Avoid'],
           prompt: "<p>Harvest or avoid?</p>",
           margin_horizontal: '16px'
-      },
+      }
    ],
-  timeline_variables: [
-      bee_chooser(rel_dim_1,rel_dim_2)
-  ],
-  randomize_order: true,
-  on_timeline_start: function() {
-    bee_chooser(rel_dim_1,rel_dim_2)
-  },
+  timeline_variables: bee_32set_builder(sample_obj),
+  randomize_order: false
 }
 
 var test = {
   timeline: [actual_trial],
-  repetitions: 2
+  repetitions: 1
 }
 
