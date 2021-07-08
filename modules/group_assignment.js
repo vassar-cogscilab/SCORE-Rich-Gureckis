9cda10c2b0d02178bcd3765ed65674e4904e432a
Miles Bader <milesdbader@gmail.com>
2021-07-08 13:35:26 -0700

    better commenting on group assignment code

--- modules/group_assignment.js
+++ modules/group_assignment.js
@@ -8,6 +8,18 @@ var dimensions = {
 var condition_assignment = jsPsych.randomization.sampleWithoutReplacement(['contingent', 'full-information'], 1)[0];
 var relevant_dimension_assignment = jsPsych.randomization.sampleWithoutReplacement(['antennae', 'wings', 'pattern', 'legs'], 2);
 
+/*
+  Takes in an array of length 2, e.g. ['antennae', 'legs']
+
+  Returns a JS Object, with 'X' as a placeholder for irrelevant dimensions.
+  e.g.
+  {
+    antennae: 'B',
+    wings: 'X',
+    pattern: 'X',
+    legs: 'F'
+  }
+*/
 var assign_bee_group = (dimensions_arr) => {
   var ret_obj = {};
   for (const [dim, options] of Object.entries(dimensions)) {
@@ -21,7 +33,7 @@ var assign_bee_group = (dimensions_arr) => {
 }
 
 jsPsych.data.addProperties({
-    condition: condition_assignment,
-    relevant_dimensions: relevant_dimension_assignment,
-    bee_group: assign_bee_group(relevant_dimension_assignment)
+    condition: condition_assignment,                           // contingent or full-information
+    relevant_dimensions: relevant_dimension_assignment,        // array of length 2, containing 2 of ['antennae', 'wings', 'pattern', 'legs']
+    bee_group: assign_bee_group(relevant_dimension_assignment) // an object as specified above
 });
