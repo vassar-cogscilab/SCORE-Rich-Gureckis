2ab05b700c6757288e3a07330dd7483cd5e90e19
rpostrowski <84097695+rpostrowski@users.noreply.github.com>
2021-07-08 11:04:49 -0700

    added a bee chooser function; inputs: rel_dims, output: block of 16

--- modules/test.js
+++ modules/test.js
@@ -116,54 +116,19 @@ var BEES = [
   }
 ]
 
-var shuffled_bees = jsPsych.randomization.repeat(BEES, 1);
-
-console.log('shuffled bees',shuffled_bees);
-
-// function for specifying relevant dimensions
-
 var rel_dim_1 = 'B'
 var rel_dim_2 = 'F'
 
-var bad_bees = shuffled_bees.filter(bee => bee.antennae == rel_dim_1 && bee.legs == rel_dim_2);
-var good_bees = shuffled_bees.filter(bee => !(bee.antennae == rel_dim_1 && bee.legs == rel_dim_2));
-
-console.log('bad bees',bad_bees);
-console.log('good bees',good_bees);
-
-var first_eight = [].concat(bad_bees.splice(0,2),(good_bees.splice(0,6)));
-var second_eight = [].concat(bad_bees.splice(0,2), good_bees.splice(0,6));
-
-// var first_eight = [].concat(bad_bees[0],good_bees[0,3]);
-// var second_eight = [].concat(bad_bees[0], good_bees[0]);
-
-// var first_eight = Array(bad_bees[0], good_bees[0,1,2,3]);
-// var second_eight = Array(bad_bees[0,1,2,3], good_bees[0,1,2,3]);
-
-console.log('first_eight',first_eight);
-console.log('second_eight',second_eight);
-console.log(bad_bees);
-console.log(good_bees);
-
-var first_eight_randomized = jsPsych.randomization.repeat(first_eight,1);
-var second_eight_randomized = jsPsych.randomization.repeat(second_eight,1);
-
-var full_list = [].concat(first_eight_randomized, second_eight_randomized);
-
-console.log(full_list);
-
-function bee_chooser() { 
-  full_list.splice(0);  
+function bee_chooser(rel_dim_1,rel_dim_2) {
+  var shuffled_bees = jsPsych.randomization.repeat(BEES, 1); //
+  var bad_bees = shuffled_bees.filter(bee => bee.antennae == rel_dim_1 && bee.legs == rel_dim_2);
+  var good_bees = shuffled_bees.filter(bee => !(bee.antennae == rel_dim_1 && bee.legs == rel_dim_2));
+  var first_eight = [].concat(bad_bees.splice(0,2),(good_bees.splice(0,6)));
+  var second_eight = [].concat(bad_bees.splice(0,2), good_bees.splice(0,6));
+  var first_eight_randomized = jsPsych.randomization.repeat(first_eight,1);
+  var second_eight_randomized = jsPsych.randomization.repeat(second_eight,1);
+  var full_list = [].concat(first_eight_randomized, second_eight_randomized);
+  return full_list
 }
 
-
-// var node = {
-//   timeline: [actual_trial],
-//   repetitions: 2
-// }
-
-// timeline.push(node);
-// var test = {
-//   timeline: [actual_trial],
-//   repetitions: 2
-// }
+console.log(bee_chooser(rel_dim_1,rel_dim_2));
